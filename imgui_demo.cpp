ce99bcba54d1a3661d4eb493c40914ad59f9300c
ocornut <omarcornut@gmail.com>
2022-06-17 15:55:59 +0200

    RangeSelect/MultiSelect: Fixed issue with Ctrl+click on TreeNode + amend demo to test drag and drop.

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -1381,8 +1381,8 @@ static void ShowDemoWindowWidgets()
             ImGui::TreePop();
         }
 
-        IMGUI_DEMO_MARKER("Widgets/Selection State/Multiple Selection (Basic)");
-        if (ImGui::TreeNode("Multiple Selection (Basic)"))
+        IMGUI_DEMO_MARKER("Widgets/Selection State/Multiple Selection (Simplified)");
+        if (ImGui::TreeNode("Multiple Selection (Simplified)"))
         {
             HelpMarker("Hold CTRL and click to select multiple items.");
             static bool selection[5] = { false, false, false, false, false };
@@ -1401,6 +1401,7 @@ static void ShowDemoWindowWidgets()
         }
 
         IMGUI_DEMO_MARKER("Widgets/Selection State/Multiple Selection (Full)");
+        //ImGui::SetNextItemOpen(true, ImGuiCond_Once);
         if (ImGui::TreeNode("Multiple Selection (Full)"))
         {
             // Demonstrate holding/updating multi-selection data and using the BeginMultiSelect/EndMultiSelect API to support range-selection and clipping.
@@ -1415,14 +1416,18 @@ static void ShowDemoWindowWidgets()
             // Test both Selectable() and TreeNode() widgets
             enum WidgetType { WidgetType_Selectable, WidgetType_TreeNode };
             static bool use_columns = false;
+            static bool use_drag_drop = true;
             static WidgetType widget_type = WidgetType_TreeNode;
             if (ImGui::RadioButton("Selectables", widget_type == WidgetType_Selectable)) { widget_type = WidgetType_Selectable; }
             ImGui::SameLine();
             if (ImGui::RadioButton("Tree nodes", widget_type == WidgetType_TreeNode)) { widget_type = WidgetType_TreeNode; }
             ImGui::SameLine();
             ImGui::Checkbox("Use 2 columns", &use_columns);
+            ImGui::SameLine();
+            ImGui::Checkbox("Use drag & drop", &use_drag_drop);
             ImGui::CheckboxFlags("io.ConfigFlags: NavEnableKeyboard", &ImGui::GetIO().ConfigFlags, ImGuiConfigFlags_NavEnableKeyboard);
             ImGui::SameLine(); HelpMarker("Hold CTRL and click to select multiple items. Hold SHIFT to select a range. Keyboard is also supported.");
+            ImGui::Text("Selection size: %d", selection.GetSelectionSize());
 
             // Open a scrolling region
             const int ITEMS_COUNT = 1000;
@@ -1468,6 +1473,11 @@ static void ShowDemoWindowWidgets()
                             ImGui::Selectable(label, item_is_selected);
                             if (ImGui::IsItemToggledSelection())
                                 selection.SetSelected(n, !item_is_selected);
+                            if (use_drag_drop && ImGui::BeginDragDropSource())
+                            {
+                                ImGui::Text("(Dragging %d items)", selection.GetSelectionSize());
+                                ImGui::EndDragDropSource();
+                            }
                         }
                         else if (widget_type == WidgetType_TreeNode)
                         {
@@ -1478,6 +1488,11 @@ static void ShowDemoWindowWidgets()
                             bool open = ImGui::TreeNodeEx(label, tree_node_flags);
                             if (ImGui::IsItemToggledSelection())
                                 selection.SetSelected(n, !item_is_selected);
+                            if (use_drag_drop && ImGui::BeginDragDropSource())
+                            {
+                                ImGui::Text("(Dragging %d items)", selection.GetSelectionSize());
+                                ImGui::EndDragDropSource();
+                            }
                             if (open)
                                 ImGui::TreePop();
                         }
